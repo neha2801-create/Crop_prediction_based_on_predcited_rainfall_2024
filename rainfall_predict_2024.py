cae029c811f4ff8981517a6c2618f96975dfe561
neha2801-create <77967216+neha2801-create@users.noreply.github.com>
2023-12-13 18:11:59 -0500

    Update rainfall_predict_2024.py

--- rainfall_predict_2024.py
+++ rainfall_predict_2024.py
@@ -15,6 +15,39 @@ dataset_file = 'India.csv'
 # Now converting it in dataframe
 dataset_df = pd.read_csv(dataset_file)
 
+#################################    Data exploration  ###################################################
+#  Converting 'datetime' column to datetime type data
+dataset_df['datetime'] = pd.to_datetime(dataset_df['datetime'])
+
+# Extracting year value from datetime type of data
+dataset_df['year'] = dataset_df['datetime'].dt.year
+
+print(f'Exploratory Data Analysis')
+print(f'Precipitation over various months through the years 2018 - 2023')
+
+# Creating respective year precipitation/raainfall level subplots subplots
+plt.figure(figsize=(22, 8))
+for year in dataset_df['year'].unique():
+    plt.subplot(2, 3, year-2018+1)  # Assuming 2018-2023
+    subset = dataset_df[dataset_df['year'] == year]
+    plt.plot(subset['datetime'], subset['precip'])
+    plt.title(f'Precipitation in {year}')
+    plt.xlabel('Datetime')
+    plt.ylabel('Precipitation')
+
+plt.tight_layout()
+plt.show()
+
+# Computing correlation matrix for demonstrating heat map for comparitive study among all column values/ parameter/ feature values
+correlation_matrix = dataset_df.corr(numeric_only=True)
+
+# Ploting heat map using correlation matrix
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Correlation Matrix')
+plt.show()
+
+
 ####################################    Filtering data based on column names further getting India specific data############
 india_data = dataset_df[dataset_df['name'] == 'India']
 
@@ -25,7 +58,7 @@ india_data['datetime'] = pd.to_datetime(india_data['datetime'])
 X_train = india_data[['datetime']]
 y_train = india_data['precip']
 
-#############################   
+#############################
 
 
 
